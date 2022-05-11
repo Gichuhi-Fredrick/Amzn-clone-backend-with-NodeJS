9a20ca9f866332191fe906ea7910be227da0dc03
Fredrick <fredgkimani@gmail.com>
2022-05-11 10:39:59 +0300

    added /home route

---
+++ controllers/home.js
@@ -0,0 +1,5 @@
+const { tacticalModel1ExtractAlpha } = require("iexjs/dist/cjs/iexjs");
+
+exports.home = async (req, res) => {
+  res.send("Its working!");
+};
--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,11 @@ const { login } = require("../controllers/login");
 const { payment } = require("../controllers/payment");
 const { orders } = require("../controllers/orders");
 const { auth } = require("../controllers/auth");
+const { home } = require("../controllers/home");
 
 // Post requests on corresponding methods
 router.all(["payment", "orders"], auth);
+router.get("/", home);
 router.post("/register", register);
 router.post("/login", login);
 router.post("/payment", payment);
