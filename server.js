796dcc9507b0abe86a314fc7d53bd707fc8d0a46
Fredrick <fredgkimani@gmail.com>
2022-05-11 21:52:08 +0300

    fixed cors

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ const user = require("./routes/user");
 const port = process.env.PORT || 5000;
 
 const app = express();
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -14,7 +23,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(cors({ origin: true }));
+// app.use(cors({ origin: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
