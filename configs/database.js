c8fa74dc9df8ef166645a30a7f23fd3488afe829
Fredrick <fredgkimani@gmail.com>
2022-05-11 10:56:45 +0300

    added connectionString

--- configs/database.js
+++ configs/database.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg");
 
+const connectionString =
+  "postgresql://dbuser:secretpassword@database.server.com:3211/mydb";
+
 const pool = new Pool({
+  connectionString: connectionString,
   user: "postgres",
   host: "localhost",
   database: "postgres",
